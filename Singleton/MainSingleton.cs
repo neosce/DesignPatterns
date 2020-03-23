0f0a7c79237a5cc7660f1bcf7a33500221007775
Mihail Akimov <akimovmi95@gmail.com>
2020-03-23 15:23:05 +0300

    Add Singleton pattern

---
+++ Singleton/MainSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class MainSingleton
+    {
+        public void Singleton()
+        {
+            Console.WriteLine("Start");
+
+            var singleton = global::Singleton.Singleton.GetInstance();
+            var singletonNew = global::Singleton.Singleton.GetInstance();
+
+            if (singleton == singletonNew)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Singleton - work!");
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Singleton - not work!");
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            
+        }
+
+    }
+}
